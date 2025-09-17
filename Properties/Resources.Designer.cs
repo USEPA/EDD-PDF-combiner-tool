0702ebb7001995fb58405f26decb03fd80f3fdac
DJAMOUS <DJAMOUS@V18H1N-DWDEV1>
2025-09-17 08:38:53 -0400

    Resources.Designer

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -6,7 +6,7 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 // </auto-generated>
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
 namespace EDD_PDF_combiner_tool.Properties {
     using System;
